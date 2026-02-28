37955a4f657f17aed906c66024ec96f38d5a3dba
derick871 <wanyamaderrick4@gmail.com>
2026-02-28 19:46:11 +0300

    create pages

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-(function){
-    const title= document getElementById (title)
-    const form = AddEventListener (submit)
+// (function){
+//     const title= document getElementById (title)
+//     const form = AddEventListener (submit)
 
+// }
+
+const div = document.getElementById('example')
+const btn = document.getElementById('submit')
+
+function handleClick() {
+    alert('Button was clicked!')
 }
+btn.addEventListener('click', handleClick)
+
+div.innerHTML +='<h1>Hello</h1>'
+
+const div =document.getElementById('hello')
+const btn =document.getElementById('submit')
+
+function handleClick() {
+    alert('button was clicked!')
+}
+btn.addEventListener('click',handleClick)
+div.innerHTML +='<h1>hello</h1>'
+
